d0e3d65cb249f9a4c71c9a086ad22fb8c6fb425a
BeardDemon <75929039+LOIC-only-one@users.noreply.github.com>
2023-10-26 09:12:01 +0000

    changement

--- TP2_LM/tp2exo8.py
+++ TP2_LM/tp2exo8.py
@@ -1,10 +1,10 @@
-x = float(input("Entrez une valeur: "))
+x = float(input("Entrez un nombre réel "))
 
-if x >= 2 and x < 3:
+if x > 2 or x == 2 and x < 3:
     print("X appartient à I")
-elif x > 0 and x <= 1:
+elif x > -10 or x == -10 and  x < -2 or x == -2:
     print("X appartient à I")
-elif x >= -10 and x <= -2:
+elif x > 0 and x < 1 or x == 1:
     print("X appartient à I")
 else:
     print("X appartient pas à I")
